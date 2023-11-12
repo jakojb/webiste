17d5b911d43d95ed46643176546114f5e525841b
jakojb <122991853+jakojb@users.noreply.github.com>
2023-11-12 10:39:12 +0100

    neu

--- pages/display.js
+++ pages/display.js
@@ -19,7 +19,7 @@ const DisplayPage = () => {
 
   const loadData = async () => {
     try {
-      const response = await fetch('/api/getData');
+      const response = await fetch('http:localhost:3001/api/getData');
       const jsonData = await response.json();
       setData(jsonData);
     } catch (error) {
